f6e59eb10bf250e33604df2f8fca6f10dbe79f06
patriciasilva <patriciasilva@deso-se.com.br>
2022-08-30 16:03:53 -0300

    alteração

--- lib/controller/bloc.dart
+++ lib/controller/bloc.dart
@@ -1,9 +1,10 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:my_cards/models/caracters_module.dart';
 
 import '../repository/api.dart';
 
-class Bloc {
-  CaractersModel caracters = CaractersModel();
+class Bloc extends BlocBase {
+  final list = BehaviorSubject<List>
   Api api = Api();
   void caracterBloc() {
     var info = api.dio;
